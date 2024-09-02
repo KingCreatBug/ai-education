c2605abf8c2432b5e2aed92cb5b46f2b0ab9be7c
KingCreatBug <trunghoang.240500@gmail.com>
2024-09-02 07:48:03 +0700

    responsive premium page

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,70 +10,6 @@ function activeLink() {
     currentValue = e.target.value;
 }
 
-//Carousel
-const carousel = document.querySelector(".feedback-container__carousel"),
-    firstItem = document.querySelectorAll(".feedback-container__box")[0],
-    arrowIcons = document.querySelectorAll(".feedback-content__btn"),
-    unMarginLeft = document.querySelector(".feedback-container");
-
-let isDragStart = false,
-    prevPageX,
-    prevScrollLeft,
-    positionDiff;
-
-let firstItemWidth = firstItem.clientWidth + 14;
-
-arrowIcons.forEach((icon) => {
-    icon.addEventListener("click", () => {
-        carousel.scrollLeft +=
-            icon.id == "left" ? -firstItemWidth : firstItemWidth;
-    });
-});
-
-const autoSlide = () => {
-    positionDiff = Math.abs(positionDiff);
-    let firstItemWidth = firstItem.clientWidth + 14;
-    let valDifference = firstItemWidth - positionDiff;
-
-    if (carousel.scrollLeft > prevScrollLeft) {
-        return (carousel.scrollLeft +=
-            positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff);
-    }
-    carousel.scrollLeft -=
-        positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff;
-};
-
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-};
-
-const dragging = (e) => {
-    if (!isDragStart) return;
-    e.preventDefault();
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    // unMarginLeft.style.marginLeft = "20px";
-};
-
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-    autoSlide();
-};
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-
-carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("mouseleave", dragStop);
-carousel.addEventListener("touchend", dragStop);
-
 // Slide text
 let copy1 = document.querySelector(".footer-slide__wrap").cloneNode(true);
 document.querySelector(".footer-slide").appendChild(copy1);
@@ -164,3 +100,72 @@ window.addEventListener("template-loaded", () => {
         };
     });
 });
+
+//Delete banner
+function deleteBanner() {
+    document.getElementById("banner").style.display = "none";
+}
+
+//Carousel
+const carousel = document.querySelector(".feedback-container__carousel"),
+    firstItem = document.querySelectorAll(".feedback-container__box")[0],
+    arrowIcons = document.querySelectorAll(".feedback-content__btn"),
+    unMarginLeft = document.querySelector(".feedback-container");
+
+let isDragStart = false,
+    prevPageX,
+    prevScrollLeft,
+    positionDiff;
+
+let firstItemWidth = firstItem.clientWidth + 14;
+
+arrowIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        carousel.scrollLeft +=
+            icon.id == "left" ? -firstItemWidth : firstItemWidth;
+    });
+});
+
+const autoSlide = () => {
+    positionDiff = Math.abs(positionDiff);
+    let firstItemWidth = firstItem.clientWidth + 14;
+    let valDifference = firstItemWidth - positionDiff;
+
+    if (carousel.scrollLeft > prevScrollLeft) {
+        return (carousel.scrollLeft +=
+            positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff);
+    }
+    carousel.scrollLeft -=
+        positionDiff > firstItemWidth / 3 ? valDifference : -positionDiff;
+};
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    // unMarginLeft.style.marginLeft = "20px";
+};
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    autoSlide();
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
